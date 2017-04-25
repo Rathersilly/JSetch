65bb3f9ebe6f5a8705df33802d47e7fbc5af062c
Rathersilly <rathersilly@live.com>
2017-04-25 15:34:08 -0600

    basic functionality sorted, needs toolbar, polish, documentation

--- main.js
+++ main.js
@@ -1,60 +1,49 @@
 $(document).ready(function(){
-	//square width = 100%/#squares
-	console.log("hi");
-	var tempoffset = 0;
-	var gridrows = 20;
-	var gridcols = 20;
-	var gridwidth = 100  / gridcols - tempoffset;
-	var gridheight = 100 / gridrows - tempoffset;
-	var red = 0; var green = 100; var blue = 100;
-	var cellcolor;
-	//199,150)";//#0055ff";
-	cellcolor = "rgb("+red+","+green+","+blue+")";
-	var hovercolor = "green";
-	for(i = 0;i <gridrows; i++) {
-		//cellcolor = "rgb("+red+","+green+","+blue+")";
-		//blue += 10;
-		//green = 100;
-		//make containing row div
-		$newrow=$("<div></div>");
-		$newrow.addClass("row");
-		$newrow.appendTo(".grid");
-		$newrow.height(gridheight + "%");
-		for(j = 0; j<gridcols; j++) {
-			//cellcolor = "rgb("+red+","+green+","+blue+")";
-			$newcell = $("<div></div>"); 
-			$newcell.width(gridwidth + "%");
-			$newcell.addClass("cell");
-			$newcell.css("background-color", cellcolor);
-			$newrow.append($newcell);
-			
+	
+	var defaultsize = 20;
+	
+	var cellcolor = "#ffffff";
+	var hovercolor = "#000000";
+	makegrid();
 
-			//green += 10;
-		};
-	};
+	
+	
+
 	$(".cell").hover(function(){
 		$(this).css("background-color", hovercolor);
 	});
 
 	$("#resetbutton").click(function(){
 		$(".cell").css("background-color", cellcolor);
-
-
-
 	});
+	console.log("here");
+	
 
+	function makegrid(size=defaultsize) {
+		console.log("making grid");
+		var cellsize = $(".grid").width() / size;
+		
+		for(i = 0;i <size; i++) {
+			console.log(cellsize);
+			$newrow=$("<div></div>");
+			$newrow.height(cellsize);
+			$newrow.addClass("row");
+			$newrow.appendTo(".grid");
+			
+			for(j = 0; j<size; j++) {
+				
+				$newcell = $("<div></div>");
+				$newcell.width(cellsize);
+				$newcell.height(cellsize); 
+				
+				$newcell.addClass("cell");
+				$newcell.css("background-color", cellcolor);
+				$newrow.append($newcell);
+				
 
-
-
-
-
-
-
-
-
-
-
-
+			}
+		};
+	}
 
 
 });
